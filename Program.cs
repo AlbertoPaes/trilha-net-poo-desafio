4088cadde4271769731d9c33e364b3d3df7c7ded
Alberto <tejotaesi@gmail.com>
2023-12-21 18:37:35 -0300

    The 'Nokia' and 'iPhone' classes are now child classes of Smartphone. Furthermore, the 'InstalarAplicativo' method is overridden in the Nokia and iPhone classes.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "0909", modelo: "Modelo 7", imei: "098765432", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Uber");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "8976", modelo: "Modelo 11", imei: "762098165", memoria: 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Skype");
